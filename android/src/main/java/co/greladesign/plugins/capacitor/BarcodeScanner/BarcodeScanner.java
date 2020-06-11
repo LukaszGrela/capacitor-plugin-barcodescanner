51d8618d5191579e84b26d4673a1b320e1cc36bb
Lukasz Grela <lukasz.grela@gmail.com>
2020-06-11 16:09:27 +0100

    Use custom request code

--- android/src/main/java/co/greladesign/plugins/capacitor/BarcodeScanner/BarcodeScanner.java
+++ android/src/main/java/co/greladesign/plugins/capacitor/BarcodeScanner/BarcodeScanner.java
@@ -21,49 +21,51 @@ import com.google.zxing.integration.android.IntentResult;
 )
 public class BarcodeScanner extends Plugin {
 
-    public static final int SCANNER_REQUEST_CODE = 1980;
+  public static final int SCANNER_REQUEST_CODE = 1980;
 
-    private static final String SCANNER_EVENT_PAYLOAD_PARAM_BARCODE = "barcode";
-    private static final String SCANNER_EVENT_PAYLOAD_PARAM_ERROR = "error";
-    private static final String SCANNER_EVENT_PAYLOAD_PARAM_CANCELLED = "cancelled";
+  private static final String SCANNER_EVENT_PAYLOAD_PARAM_BARCODE = "barcode";
+  private static final String SCANNER_EVENT_PAYLOAD_PARAM_ERROR = "error";
+  private static final String SCANNER_EVENT_PAYLOAD_PARAM_CANCELLED = "cancelled";
 
-    private static final String SCANNER_EVENT_NAME =
-        "co.greladesign.plugins.capacitor.event.EVENT_BARCODE_SCANNED";
+  private static final String SCANNER_EVENT_NAME =
+      "co.greladesign.plugins.capacitor.event.EVENT_BARCODE_SCANNED";
 
-    @PluginMethod()
-    public void scan(PluginCall call) {
-        saveCall(call);
+  @PluginMethod()
+  public void scan(PluginCall call) {
+    saveCall(call);
 
-        final IntentIntegrator integrator = new IntentIntegrator(this.getBridge().getActivity())
-            .setOrientationLocked(false)
-            .setCaptureActivity(ScannerActivity.class);
-        final Intent intent  =  integrator.createScanIntent();
+    final IntentIntegrator integrator = new IntentIntegrator(this.getBridge().getActivity())
+        .setRequestCode(SCANNER_REQUEST_CODE)
+        .setOrientationLocked(false)
+        .setCaptureActivity(ScannerActivity.class);
+    final Intent intent = integrator.createScanIntent();
 
-        startActivityForResult(call, intent, SCANNER_REQUEST_CODE);
+    startActivityForResult(call, intent, SCANNER_REQUEST_CODE);
+  }
+
+  @Override
+  protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
+    super.handleOnActivityResult(requestCode, resultCode, data);
+    // Get the previously saved call
+    PluginCall savedCall = getSavedCall();
+    if (savedCall == null) {
+      return;
     }
 
-    @Override
-    protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
-        super.handleOnActivityResult(requestCode, resultCode, data);
-        // Get the previously saved call
-        PluginCall savedCall = getSavedCall();
+    if (requestCode == SCANNER_REQUEST_CODE) {
+      IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
 
-        if (savedCall == null) {
-            return;
+      if (result != null) {
+        final JSObject message = new JSObject();
+        if (result.getContents() == null) {
+          // cancelled
+          message.put(SCANNER_EVENT_PAYLOAD_PARAM_CANCELLED, true);
+        } else {
+          String st_scanned_result = result.getContents();
+          message.put(SCANNER_EVENT_PAYLOAD_PARAM_BARCODE, st_scanned_result);
         }
-
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-
-        if(result != null) {
-            final JSObject message = new JSObject();
-            if(result.getContents() == null) {
-                // cancelled
-                message.put(SCANNER_EVENT_PAYLOAD_PARAM_CANCELLED, true);
-            } else {
-                String st_scanned_result = result.getContents();
-                message.put(SCANNER_EVENT_PAYLOAD_PARAM_BARCODE, st_scanned_result);
-            }
-            notifyListeners(SCANNER_EVENT_NAME, message);
-        }
+        notifyListeners(SCANNER_EVENT_NAME, message);
+      }
     }
+  }
 }
