481ed18803749945a149141c98f8350bf62fd424
Lukasz Grela <lukasz.grela@gmail.com>
2020-06-11 13:55:57 +0100

    Call custom scanner activity

--- android/src/main/java/co/greladesign/plugins/capacitor/BarcodeScanner/BarcodeScanner.java
+++ android/src/main/java/co/greladesign/plugins/capacitor/BarcodeScanner/BarcodeScanner.java
@@ -14,9 +14,14 @@ import com.google.zxing.integration.android.IntentResult;
 @NativePlugin(
     permissions = {
         Manifest.permission.CAMERA
+    },
+    requestCodes = {
+        BarcodeScanner.SCANNER_REQUEST_CODE
     }
 )
 public class BarcodeScanner extends Plugin {
+
+    public static final int SCANNER_REQUEST_CODE = 1980;
 
     private static final String SCANNER_EVENT_PAYLOAD_PARAM_BARCODE = "barcode";
     private static final String SCANNER_EVENT_PAYLOAD_PARAM_ERROR = "error";
@@ -27,11 +32,25 @@ public class BarcodeScanner extends Plugin {
 
     @PluginMethod()
     public void scan(PluginCall call) {
-        new IntentIntegrator(this.getBridge().getActivity()).initiateScan();
+        saveCall(call);
+
+        final IntentIntegrator integrator = new IntentIntegrator(this.getBridge().getActivity())
+            .setOrientationLocked(false)
+            .setCaptureActivity(ScannerActivity.class);
+        final Intent intent  =  integrator.createScanIntent();
+
+        startActivityForResult(call, intent, SCANNER_REQUEST_CODE);
     }
 
     @Override
     protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
+        super.handleOnActivityResult(requestCode, resultCode, data);
+        // Get the previously saved call
+        PluginCall savedCall = getSavedCall();
+
+        if (savedCall == null) {
+            return;
+        }
 
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 
